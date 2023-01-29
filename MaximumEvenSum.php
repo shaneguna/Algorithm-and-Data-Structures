55b7621c5279bb73818c66611c040f4ce9ae9fc6
Shane Gunawardana <shanechioguna@gmail.com>
2023-01-29 20:56:22 +0800

    Create MaximumEvenSum.php

---
+++ MaximumEvenSum.php
@@ -0,0 +1,31 @@
+<?php
+
+function getMaxiumEvenSum($input){
+ $sum = 0;
+ $count = count($input);
+ 
+ for ($i = 0; $i < $count; ++$i) {
+   if ($input[$i] > 0) {
+     $sum += $input[$i];
+   }
+ }
+ 
+ if ($sum % 2 === 0) {
+   return $sum;
+ }
+ 
+ $result = PHP_INT_MIN;
+ 
+ for ($i = 0; $i < $count; ++$i) {
+   if ($input[$i] %2 !== 0) {
+     if ($input[$i] > 0) {
+       $result = max($ans, $sum - $input[$i]);
+      
+     }else {
+       $result = max($ans, $sum + $input[$i]);
+     }
+   }
+ }
+ 
+ return $result;
+}
